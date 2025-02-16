d5080f9f018acffdac74274b465b50d9f01202fd
Aiden <x29587629@gmail.com>
2025-02-16 16:36:19 +0800

    add friend apply content

--- src/features/chat/ChatPage.tsx
+++ src/features/chat/ChatPage.tsx
@@ -1,4 +1,5 @@
 import type React from "react"
+import { useState } from "react"
 import { useEffect } from "react"
 import {
   Paper,
@@ -9,7 +10,13 @@ import {
   Avatar,
   Divider,
   Typography,
+  IconButton,
+  Dialog,
+  Tabs,
+  Tab, Button,
 } from "@mui/material"
+import AddIcon from "@mui/icons-material/Add"
+import VisibilityIcon from "@mui/icons-material/Visibility"
 import { Chat } from "@mui/icons-material"
 import { useAppDispatch, useAppSelector } from "../../app/hooks"
 import {
@@ -17,7 +24,9 @@ import {
   loadFriends,
   loadGroups,
   selectFriendInfoList,
-  selectRoomInfoList, setCurrentRoomId, setType,
+  selectRoomInfoList,
+  setCurrentRoomId,
+  setType,
 } from "./ChatPageSlice"
 import { selectTokenInfo } from "../common/globalSlice"
 import ChatBox from "./components/ChatBox"
@@ -27,6 +36,13 @@ const ChatPage: React.FC = () => {
   const friendInfos = useAppSelector(selectFriendInfoList)
   const roomInfos = useAppSelector(selectRoomInfoList)
   const tokenInfo = useAppSelector(selectTokenInfo)
+  const [openDialog, setOpenDialog] = useState(false)
+  const [tabIndex, setTabIndex] = useState(0)
+
+  const handleOpenDialog = () => setOpenDialog(true)
+  const handleCloseDialog = () => setOpenDialog(false)
+  const handleChangeTab = (event: any, newValue: number) =>
+    setTabIndex(newValue)
 
   useEffect(() => {
     dispatch(loadFriends({ friendUserIds: [] }))
@@ -38,6 +54,22 @@ const ChatPage: React.FC = () => {
     dispatch(setCurrentRoomId(roomId))
     dispatch(loadChats({ roomId: roomId }))
   }
+
+  const friendApplyList = friendInfos.map(info => {
+    return (
+      <ListItem
+        sx={{ marginBottom: 1 }}
+        key={"friend_" + info.id}
+      >
+        <Avatar sx={{ marginRight: 2 }}>{info.showName.charAt(0)}</Avatar>
+        <ListItemText primary={info.showName} secondary={info.friendUserId} />
+        <Button sx={{ bgcolor: "green", color: "white", marginRight: 1 }}>
+          Accept
+        </Button>
+        <Button sx={{ bgcolor: "red", color: "white" }}>Reject</Button>
+      </ListItem>
+    )
+  })
 
   const friendList = friendInfos.map(info => {
     return (
@@ -81,16 +113,67 @@ const ChatPage: React.FC = () => {
           borderRadius: 2,
         }}
       >
-        <Typography variant="h6" gutterBottom>
-          Personal Chats
-        </Typography>
-        <List>{friendList}</List>
+        {/* Friend Request Box */}
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+          <Typography variant="h6">Friend Apply</Typography>
+        </Box>
+        <List>{friendApplyList}</List>
+
         <Divider sx={{ my: 2, bgcolor: "#282c34" }} />
-        <Typography variant="h6" gutterBottom>
-          Group Chats
-        </Typography>
-        <List>{roomList}</List>
+
+        {/* Personal Chats Header */}
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+          <Typography variant="h6">Personal Chats</Typography>
+          <Box>
+            <IconButton sx={{ color: "white" }} size="small">
+              <AddIcon />
+            </IconButton>
+            <IconButton
+              sx={{ color: "white" }}
+              size="small"
+              onClick={handleOpenDialog}
+            >
+              <VisibilityIcon />
+            </IconButton>
+          </Box>
+        </Box>
+        <List>{friendList.slice(0, 6)}</List>
+        <Divider sx={{ my: 2, bgcolor: "#282c34" }} />
+
+        {/* Group Chats Header */}
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+          <Typography variant="h6">Group Chats</Typography>
+          <Box>
+            <IconButton sx={{ color: "white" }} size="small">
+              <AddIcon />
+            </IconButton>
+            <IconButton
+              sx={{ color: "white" }}
+              size="small"
+              onClick={handleOpenDialog}
+            >
+              <VisibilityIcon />
+            </IconButton>
+          </Box>
+        </Box>
+        <List>{roomList.slice(0, 6)}</List>
       </Paper>
+
+      {/* Dialog for viewing all contacts and chats */}
+      <Dialog
+        open={openDialog}
+        onClose={handleCloseDialog}
+        fullWidth
+        maxWidth="sm"
+      >
+        <Tabs value={tabIndex} onChange={handleChangeTab} variant="fullWidth">
+          <Tab label="我的好友" />
+          <Tab label="我的聊天室" />
+        </Tabs>
+        <Box p={2}>
+          {tabIndex === 0 ? <List>{friendList}</List> : <List>{roomList}</List>}
+        </Box>
+      </Dialog>
 
       <ChatBox />
     </Box>
