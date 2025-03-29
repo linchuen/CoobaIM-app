08252d52df8accc78ffcdffbc5c3e7d42770f94c
Aiden <x29587629@gmail.com>
2025-03-29 13:52:45 +0800

    update nginx

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,9 +1,9 @@
 import { RoleType } from "../services/constant"
 
 const globalConfig = {
-  apiUrl: "http://127.0.0.1:8080",
-  wsUrl: "ws://127.0.0.1:8080",
-  livekitUrl: "http://127.0.0.1:7880",
+  apiUrl: process.env.REACT_APP_API_URL || "http://127.0.0.1:8080",
+  wsUrl:  process.env.REACT_APP_WEBSOCKET || "ws://127.0.0.1:8080",
+  livekitUrl: process.env.REACT_APP_LIVEKIT || "http://127.0.0.1:7880",
   appName: "Cooba IM App",
   useFake: false,
   testRole: RoleType.USER,
