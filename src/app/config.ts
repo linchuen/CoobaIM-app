5b6383e6621ffd5603e327af50e6b0385fda130a
Aiden <x29587629@gmail.com>
2025-03-29 23:53:11 +0800

    update env

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,9 +1,9 @@
 import { RoleType } from "../services/constant"
 
 const globalConfig = {
-  apiUrl: process.env.REACT_APP_API_URL || "http://127.0.0.1:8080",
-  wsUrl:  process.env.REACT_APP_WEBSOCKET || "ws://127.0.0.1:8080",
-  livekitUrl: process.env.REACT_APP_LIVEKIT || "http://127.0.0.1:7880",
+  apiUrl: import.meta.env.VITE_API_URL ,
+  wsUrl:  import.meta.env.VITE_APP_WEBSOCKET ,
+  livekitUrl: import.meta.env.VITE_APP_LIVEKIT,
   appName: "Cooba IM App",
   useFake: false,
   testRole: RoleType.USER,
--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,21 @@ export default defineConfig({
   plugins: [react()],
   server: {
     open: true,
+    proxy: {
+      '/ws': {
+        target: 'ws://localhost:8080', // 後端 WebSocket server
+        ws: true,                      // 啟用 WebSocket 代理
+        changeOrigin: true,
+      },
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '') // 可選：重寫路徑
+      },
+      'livekit': {
+        target: 'http://localhost',
+      }
+    }
   },
   build: {
     outDir: 'dist', // 打包輸出的資料夾，可自訂
